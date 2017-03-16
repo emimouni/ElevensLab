e2b5f8d0c30a9378b7ef66b1c77a25d899e32f21
emimouni <emimouni10@hotmail.com>
2017-03-16 10:31:14 -0400

    Initial commit

---
+++ src/com/company/CardTester.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by em343 on 3/15/17.
+ */
+public class CardTester {
+
+    /**
+     * The main method in this class checks the Card operations for consistency.
+     *	@param args is not used.
+     */
+    public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+    }
+}
