9d526f281c0905eccc1880e554c9b2b83f46c47c
Elias Mimouni <emimouni10@hotmail.com>
2017-03-22 06:34:33 -0400

    Update CardTester.java

--- src/com/company/CardTester.java
+++ src/com/company/CardTester.java
@@ -1,3 +1,4 @@
+
 package com.company;
 
 /**
@@ -11,5 +12,27 @@ public class CardTester {
      */
     public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	    	Card c1 = new Card("Queen", "Hearts" , 12);
+		Card c2 = new Card("Ace", "Spades" , 1);
+		Card c3 = new Card("Jack","Dimonds",11 );
+
+
+		System.out.println(card.rank());
+		System.out.println(card.suit());
+		System.out.println(card.pointValue());
+		System.out.println(card.matches(c1));
+		System.out.println(card.toString());
+
+		System.out.println(card1.rank());
+		System.out.println(card1.suit());
+		System.out.println(card1.pointValue());
+		System.out.println(card1.matches(c2));
+		System.out.println(card1.toString());
+
+		System.out.println(card2.rank());
+		System.out.println(card2.suit());
+		System.out.println(card2.pointValue());
+		System.out.println(card2.matches(c3));
+		System.out.println(card2.toString());
     }
 }
