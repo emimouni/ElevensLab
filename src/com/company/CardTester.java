211ced47f0b43f72aaa085959666e40b9da8e131
Elias Mimouni <emimouni10@hotmail.com>
2017-04-02 18:46:27 -0400

    Update CardTester.java

--- src/com/company/CardTester.java
+++ src/com/company/CardTester.java
@@ -1,9 +1,8 @@
-
+/**
+ * This is a class that tests the Card class.
+ */
 package com.company;
 
-/**
- * Created by em343 on 3/15/17.
- */
 public class CardTester {
 
     /**
@@ -11,28 +10,29 @@ public class CardTester {
      *	@param args is not used.
      */
     public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	    	Card c1 = new Card("Queen", "Hearts" , 12);
-		Card c2 = new Card("Ace", "Spades" , 1);
-		Card c3 = new Card("Jack","Dimonds",11 );
+		Card card1 = new Card("Jack", "Hearts", 11);
+        Card card2 = new Card("Queen", "Clubs", 12);
+        Card card3 = new Card("King", "Spades", 13);
+
+        System.out.println(card1.rank());
+        System.out.println(card1.suit());
+        System.out.println(card1.pointValue());
+        System.out.println(card1.matches(card2));
+        System.out.println(card1.toString() +"\n");
 
 
-		System.out.println(card.rank());
-		System.out.println(card.suit());
-		System.out.println(card.pointValue());
-		System.out.println(card.matches(c1));
-		System.out.println(card.toString());
+        System.out.println(card2.rank());
+        System.out.println(card2.suit());
+        System.out.println(card2.pointValue());
+        System.out.println(card2.matches(card3));
+        System.out.println(card2.toString()+"\n");
 
-		System.out.println(card1.rank());
-		System.out.println(card1.suit());
-		System.out.println(card1.pointValue());
-		System.out.println(card1.matches(c2));
-		System.out.println(card1.toString());
 
-		System.out.println(card2.rank());
-		System.out.println(card2.suit());
-		System.out.println(card2.pointValue());
-		System.out.println(card2.matches(c3));
-		System.out.println(card2.toString());
+        System.out.println(card3.rank());
+        System.out.println(card3.suit());
+        System.out.println(card3.pointValue());
+        System.out.println(card3.matches(card1));
+        System.out.println(card3.toString());
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
     }
 }
